e09f00ee0bcf25123e71e5720593a4a5740d1002
Barreira.Tom <tomas.barreira14@outlook.pt>
2022-05-08 21:37:00 +0100

    Updated keyboardInputTest with new, more relevant packages

--- Server/keyboardInputTest.py
+++ Server/keyboardInputTest.py
@@ -1,34 +1,107 @@
 #
 # ONLY WORKS ON LINUX!!!!
 #
-import curses
 
-# Get the curses window, turn off echoing of keyboard to screen, turn on
-# instant (no waiting) key response, and use special values for cursor keys
-screen = curses.initscr()
-curses.noecho()
-curses.cbreak()
-screen.keypad(True)
+from re import U
+import sys
+import time
 
-try:
-    while True:
-        char = screen.getch()
-        if char == ord('q'):
-            break
-        elif char == curses.KEY_UP:
-            print ("up\n")
-        elif char == curses.KEY_DOWN:
-            print ("down\n")
-        elif char == curses.KEY_RIGHT:
-            print ("right\n")
-        elif char == curses.KEY_LEFT:
-            print ("left\n")
-        elif char == 10:
-            print ("stop\n")
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
-finally:
-    # Close down curses properly, inc turn echo back on!
-    curses.nocbreak()
-    screen.keypad(0)
-    curses.echo()
-    curses.endwin()
+class Ui_Client(object):
+    def setupUi(self, Client):
+        Client.resize(600, 100)
+        Client.setWindowTitle('Keyboard Window Test')
+        font = QtGui.QFont()
+        font.setFamily("3ds")
+        font.setPointSize(40)
+        Client.setFont(font)
+
+        self.W = QtWidgets.QLabel(Client)
+        self.W.setGeometry(QtCore.QRect(0, 0, 100, 100))
+        self.W.setText("W")
+        self.W.setVisible(False)
+
+        self.A = QtWidgets.QLabel(Client)
+        self.A.setGeometry(QtCore.QRect(100, 0, 100, 100))
+        self.A.setText("A")
+        self.A.setVisible(False)
+
+        self.S = QtWidgets.QLabel(Client)
+        self.S.setGeometry(QtCore.QRect(200, 0, 100, 100))
+        self.S.setText("S")
+        self.S.setVisible(False)
+
+        self.D = QtWidgets.QLabel(Client)
+        self.D.setGeometry(QtCore.QRect(300, 0, 100, 100))
+        self.D.setText("D")
+        self.D.setVisible(False)
+
+
+class mywindow(QMainWindow, Ui_Client):
+    def __init__(self):
+        global timer
+        super(mywindow, self).__init__()
+        self.setupUi(self)
+
+        self.Key_W = False
+        self.Key_A = False
+        self.Key_S = False
+        self.Key_D = False
+
+    def keyPressEvent(self, event):
+        if event.isAutoRepeat():
+            pass
+        else :
+            if event.key() == Qt.Key_W:
+                print('W')
+                self.Key_W = True
+            elif event.key() == Qt.Key_S:
+                print('S')
+                self.Key_S = True
+            elif event.key() == Qt.Key_A:
+                print('A')
+                self.Key_A = True
+            elif event.key() == Qt.Key_D:
+                print('D')
+                self.Key_D = True
+
+        self.W.setVisible(self.Key_W)
+        self.A.setVisible(self.Key_A)
+        self.S.setVisible(self.Key_S)
+        self.D.setVisible(self.Key_D)
+
+    def keyReleaseEvent(self, event):
+
+        if(event.key() == Qt.Key_W):
+            time.sleep(0.05)
+            if(event.key() == Qt.Key_W):
+                if not(event.isAutoRepeat()) and self.Key_W == True:
+                    print('-W')
+                    self.Key_W = False
+        elif(event.key() == Qt.Key_A):
+            if not(event.isAutoRepeat()) and self.Key_A == True:
+                print('-A')
+                self.Key_A = False
+        elif(event.key() == Qt.Key_S):
+            if not(event.isAutoRepeat()) and self.Key_S == True:
+                print('-S')
+                self.Key_S = False
+        elif(event.key() == Qt.Key_D):
+            if not(event.isAutoRepeat()) and self.Key_D == True:
+                print('-D')
+                self.Key_D = False
+
+        self.W.setVisible(self.Key_W)
+        self.A.setVisible(self.Key_A)
+        self.S.setVisible(self.Key_S)
+        self.D.setVisible(self.Key_D)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myshow = mywindow()
+    myshow.show()
+    sys.exit(app.exec_())
